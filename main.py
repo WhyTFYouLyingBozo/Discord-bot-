d5ad507cf7cc2540e1b1bf8dd097321ad9ad7506
Jaiy bozzo <100237050+WhyTFYouLyingBozo@users.noreply.github.com>
2023-03-20 20:19:46 +0000

    n

--- main.py
+++ main.py
@@ -1,45 +1,25 @@
-import discord
+import discord, os
 import random
 from discord.ext import commands
 
-TOKEN = "MTA4MDk2MDkwNjgxMDM3NjMxMw.GkxzzA.7xhMBR2W0tKvDY7wuIb2OmMSJDzBDZQ9LDLy9k"
+TOKEN = os.environ['token']
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix = "$", intents = intents)
-
-
-
-
-bot.run(TOKEN)
-
-"""
-import os
-import discord
-import random
-from discord.ext import commands
-
-TOKEN = os.environ['TOKEN']
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 
 
 class rng:
-
   def __init__(self):
-    self.secret_number = random.randint(0, 1000)
+    self.secret_number = random.randint(0,1000)
 
   def reset(self):
-    self.secret_number = random.randint(0, 1000)
-
+    self.secret_number = random.randint(0,1000)
 
 random_number = rng()
 
-
-@client.command(name="guess")
+@bot.command(name="guess")
 async def guess(ctx, num):
   if int(num) == random_number:
     await ctx.channel.send("Correct. I love you Finn Abey <3")
@@ -49,5 +29,5 @@ async def guess(ctx, num):
     await ctx.channel.send("Guess Higher")
 
 
-client.run(TOKEN)
-"""
+
+
