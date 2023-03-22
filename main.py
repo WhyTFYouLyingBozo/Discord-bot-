14d4232aa937375b3bf57269f26cbf8fd9da6ed2
Jaiy bozzo <100237050+WhyTFYouLyingBozo@users.noreply.github.com>
2023-03-22 23:22:37 +0000

    added a joke bot

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
+#importing packages and discord bots to get the program running
 import discord, os
 import random
 from discord.ext import commands
+from jokeapi import Jokes
 
-TOKEN = os.environ['token']
+#token for the discord botT
+TOKEN = os.environ['TOKEN']
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+#the prefix used before command for the command to work
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-
+#picking a random number for the user to guess
 class rng:
+
   def __init__(self):
-    self.secret_number = random.randint(0,1000)
+    self.secret_number = random.randint(0, 1000)
 
   def reset(self):
-    self.secret_number = random.randint(0,1000)
+    self.secret_number = random.randint(0, 1000)
+
 
 random_number = rng()
 
+#code for user to correctly guess the mystery number giving hints to help them get closer
 @bot.command(name="guess")
 async def guess(ctx, num):
-  if int(num) == random_number:
-    await ctx.channel.send("Correct. I love you Finn Abey <3")
+  if int(num) == random_number.secret_number:
+    await ctx.channel.send("Moto Moto loves you <3")
+    random_number.reset()
   elif int(num) > random_number.secret_number:
-    await ctx.channel.send("Guess Lower")
+    await ctx.channel.send("Guess Lower monkey")
   else:
-    await ctx.channel.send("Guess Higher")
+    await ctx.channel.send("Guess Higher monkey")
 
 
+#this code is for a bot who tells jokes when the user asks
+@bot.command()
+async def joke(ctx: commands.Context):
+  j = await Jokes()
+  blacklist = ["racist"]
+  if not ctx.message.channel.is_nsfw():
+    blacklist.append("nsfw")
+  joke = await j.get_joke(blacklist=blacklist)
+  msg = ""
+  if joke["type"] == "single":
+    msg = joke["joke"]
+  else:
+    msg = joke["setup"]
+    msg += f"||{joke['delivery']}||"
+  await ctx.send(msg)
 
 
+bot.run(TOKEN)
